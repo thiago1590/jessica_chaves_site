8f5ecf56229069c97f31424149d84b41a19402f8
Thiago <thiagoandre2121@gmail.com>
2021-07-15 16:07:06 -0300

    Testing sending email

--- src/routes/paymentsRoute.js
+++ src/routes/paymentsRoute.js
@@ -3,9 +3,16 @@ const routes = express.Router();
 require('dotenv').config()
 
 const paymentsController = require('../controllers/paymentsController');
+const sendEmail = require('../services/sendEmailTesting');
 
 routes.get('/payments/checkout/:id/:email/:description/:amount', paymentsController.checkout) //remover description e amount qnd terminar de testar
 routes.get('/purchases', paymentsController.listPurchases)
 routes.post('/listenPayment', paymentsController.listenPurchase)
+
+//testing
+routes.get('/sendEmail', (req,res) => {
+    sendEmail()
+    res.send('ok')
+})
 
 module.exports = routes;
