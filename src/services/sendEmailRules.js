1b56a3463d4c53037443a7a2c46592d28e5bf422
Thiago <thiagoandre2121@gmail.com>
2021-07-17 01:52:25 -0300

    Fixed some logic in controller

--- src/services/sendEmailRules.js
+++ src/services/sendEmailRules.js
@@ -4,13 +4,24 @@ const { v4 } = require('uuid')
 
 async function sendEmail(id, buyer_email, status) {
 
+  if (!await knex('purchase').where({ id_payment: id })) {
+    await knex('purchase').insert({
+      id: v4(),
+      id_payment: id,
+      buyer_email,
+      first_email_sent: 0,
+      second_email_sent: 0,
+      status: 'not_approved'
+    })
+  }
+
   try {
     var [{ first_email_sent, second_email_sent }] =
       await knex('purchase')
         .select('first_email_sent', 'second_email_sent')
         .where({ id_payment: id })
   } catch (err) {
-    err.message
+    console.log(err.message)
   }
 
   if (status == 'pending') {
@@ -18,14 +29,7 @@ async function sendEmail(id, buyer_email, status) {
       mailer(buyer_email, "first")
 
       try {
-        await knex('purchase').insert({
-          id: v4(),
-          id_payment: id,
-          buyer_email,
-          first_email_sent: 1,
-          second_email_sent: 0,
-          status
-        })
+        await knex('purchase').update({ first_email_sent: 1 }).where({ id_payment: id })
         return 'sent first email'
       } catch (err) {
         return err.message
@@ -36,25 +40,7 @@ async function sendEmail(id, buyer_email, status) {
 
   if (status == 'approved') {
     if (second_email_sent == 0) {
-      console.log('cheguei aki')
       mailer(buyer_email, "second")
-
-      if (!await knex('purchase').where({ id_payment: id })) {
-        try {
-          await knex('purchase').insert({
-            id: v4(),
-            id_payment: id,
-            buyer_email,
-            first_email_sent: 1,
-            second_email_sent: 1,
-            status
-          })
-          return 'sent second email'
-        }
-        catch (err) {
-          return err.message
-        }
-      }
 
       try {
         await knex('purchase').update({ second_email_sent: 1 }).where({ id_payment: id })
