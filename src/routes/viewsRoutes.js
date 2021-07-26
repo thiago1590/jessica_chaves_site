363ef2b0f06cd6431465a47fc38fd6cda975f214
Thiago <thiagoandre2121@gmail.com>
2021-07-26 14:32:35 -0300

    Code refactoring to improve scalability

--- src/routes/viewsRoutes.js
+++ src/routes/viewsRoutes.js
@@ -2,19 +2,19 @@ const express = require('express');
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-    return res.status(200).render("html/home.html")
+    return res.status(200).render("html/ebook/home.html")
 })
 routes.get('/register', (req, res) => {
-    return res.render("html/register.html")
+    return res.render("html/ebook/register.html")
 })
 routes.get('/payments/success', (req, res) => {
-    return res.render("html/success.html")
+    return res.render("html/ebook/success.html")
 })
 routes.get('/payments/pending', (req, res) => {
-    return res.render("html/pending.html")
+    return res.render("html/ebook/pending.html")
 })
 routes.get('/payments/failure', (req, res) => {
-    return res.render("html/failure.html")
+    return res.render("html/ebook/failure.html")
 })
 
 
